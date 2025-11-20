e2fb2bd589535ff6c15080c47d1bf35b824255a5
Ujjwal Tiwari <158622173+Ujjwaltiwari08@users.noreply.github.com>
2025-11-21 02:46:42 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Hero from "./components/Hero";
 import Features from "./components/Features";
 import Footer from "./components/Footer";
 import BrandCard from "./components/Brandcard";
-import HorizontalScroll from "./components/Horizontalscroll";
+import HorizontalScroll from "./components/HorizontalScroll";
 import Qualityproduct from "./components/Qualityproduct";
 import CreateStoreButton from "./components/CreateStoreButton";
 
