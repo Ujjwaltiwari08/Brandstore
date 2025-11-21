fcee01a7d960da2109c916a88709ea59d8dd9789
Ujjwal Tiwari <158622173+Ujjwaltiwari08@users.noreply.github.com>
2025-11-21 18:40:49 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,1 @@
-import React from "react";
 
-const Footer = () => {
-  return (
-    <footer className="w-full text-center p-3 bg-black text-white mt-8 text-xs sm:p-6 sm:mt-10 sm:text-sm">
-      © 2025 Selectvia | All Rights Reserved
-    </footer>
-  );
-};
-
-export default Footer;
